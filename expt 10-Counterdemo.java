b193172110ac4455e1fba343aff30978001f7190
21CSE068 <111351992+21CSE068@users.noreply.github.com>
2022-10-26 22:03:44 +0530

    Update expt 10-Counterdemo.java

--- expt 10-Counterdemo.java
+++ expt 10-Counterdemo.java
@@ -29,7 +29,7 @@ public class Counterdemo extends JFrame implements ActionListener {
          repaint();
       } 
    }
-   
+  
    public static void main(String[] args) {
       new Counterdemo();
    }
